dc376bbb6ca0ad2f0c5ff5194b6fdf9ee2cb9838
vagharsh <37323727+vilnyus@users.noreply.github.com>
2019-03-27 21:51:09 +0400

    Update controller.js

--- lib/controller.ts
+++ lib/controller.ts
@@ -1,23 +1,58 @@
+import * as mongoose from 'mongoose';
+import { AddressSchema } from '../models/addressModel';
+import { Request, Response } from 'express';
+
+const Address = mongoose.model('Address', AddressSchema); //Why out of class AddressController
 
 export class AddressController {
-  
-  public addNewAddress() {
-  
-  }
-  
-  public getAllAddresses() {
-  
-  }
-  
-  public getAddressById() {
-  
-  }
-  
-  public updateAddress() {
+
+  public addNewAddress(req: Request, res: Response) {
+    let newAddress = new Address(req.body);
+    
+    newAddress.save((err, address)=>{
+      if(err){
+        res.send(err);
+      }
+      res.json(address);
+    });    
   
   }
   
-  public deleteAddressById(){
+  public getAllAddresses(req: Request, res: Response) {
+    Address.find({}, (err, addressList)=> {
+      if(err) {
+        res.send(err);
+      }
+      res.json(addressList);
+    });  
+  }
+  
+  public getAddressById(req: Request, res: Response) {
+    Address.findOne(req.params.addressId, (err, address)=> { 
+      if(err) {
+        res.send(err);
+      }
+      res.json(address)
+    });  
+  }
+  
+  public updateAddress(req: Request, res: Response) {
+    Address.findOneAndUpdate({_id: req.params.addressId}, (err, address)=> {
+      if(err) {
+        res.send(err);
+      }
+      res.json(address);
+    });
+  
+  }
+  
+  public deleteAddressById(req: Request, res: Response) {
+    Address.remove({_id: req.params.addressId}, (err, address)=> {
+      if(err) {
+        res.send(err);
+      }
+      res.json(address);
+    });
   
   }  
 }
