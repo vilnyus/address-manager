c4123a2b4f0b491b3728e6f1c866358b64ce0e79
vagharsh <37323727+vilnyus@users.noreply.github.com>
2019-03-27 22:02:24 +0400

    Update schema.ts
    added Address list schema 
    first name
    last name 
    street 
    home
    apartment
    tel number
    create date

--- models/addressModel.ts
+++ models/addressModel.ts
@@ -1,1 +1,41 @@
+import * as mongoose from 'mongoose';
 
+const Schema = mongoose.Schema;
+
+export const AddressSchema = new Schema({
+  firstName: {
+    type: string,
+    required: 'Enter first name'
+  },
+  
+  lastName: {
+    type: string,
+    required: 'Enter second name'
+  },
+  
+  streetName: {
+    type: string,
+    required: 'Enter street name'
+  },
+  
+  homeNumber: {
+    type: number,
+    required: 'ENter home number'
+  },
+  
+  aptNumber: {
+    type: number,
+    required: 'Enter apartment number'
+  },
+  
+  telNumber: {
+    type: number
+  },
+  
+  createDate: {
+    type: Date,
+    default: Date.now
+  }
+  
+  
+});
