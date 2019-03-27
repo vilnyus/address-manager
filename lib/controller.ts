698388916f4cf0ce91cba85e928def889e606d1c
vagharsh <37323727+vilnyus@users.noreply.github.com>
2019-03-27 21:20:45 +0400

    Update controller.ts

--- lib/controller.ts
+++ lib/controller.ts
@@ -1,1 +1,23 @@
 
+export class AddressController {
+  
+  public addNewAddress() {
+  
+  }
+  
+  public getAllAddresses() {
+  
+  }
+  
+  public getAddressById() {
+  
+  }
+  
+  public updateAddress() {
+  
+  }
+  
+  public deleteAddressById(){
+  
+  }  
+}
