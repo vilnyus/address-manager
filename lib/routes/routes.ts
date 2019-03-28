7b64f47a2b94e05d688db4cf8de4c7dd3746467b
vagharsh <37323727+vilnyus@users.noreply.github.com>
2019-03-28 11:49:50 +0400

    Update route.ts
    adding route class
    adding route to home page, to /address , to /address/:addressID

--- lib/routes/routes.ts
+++ lib/routes/routes.ts
@@ -1,1 +1,23 @@
 
+import { Request, Response } from "express";
+import { AddressController } from "../controllers/controller.ts";
+
+export class Routes {
+  public addressController: AddressController = new AddressController();
+  public routes(app): void {
+    
+    app.route('/') //Route to home page
+      .get((req: Request, res: Response) => {
+        res.status(200).send({message: "Home page"});        
+      })
+    
+    app.route('/address')
+      .get(this.addressController.getAllAddresses) // Show all addresses list 
+      .post(this.addressController.addNewAddress) //Add address
+    
+    add.route('/address/:addressID') // Editing contact list by ID
+      .get(this.addressController.getAddressById) // Get special address useing ID 
+      .put(this.addressController.updateAddress)  // Update address useing ID
+      .delete(this.addressController.deleteAddressById)  // Delete address useing ID
+  } 
+}
